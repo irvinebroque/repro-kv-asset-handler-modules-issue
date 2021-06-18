08cc52726ee1477be5b7ef2d3807d130d5623ee8
Brendan Irvine-Broque <brendan@Brendans-Air.localdomain>
2021-06-18 11:52:00 -0700

    show empty STATIC_CONTENT object

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ refs https://github.com/cloudflare/kv-asset-handler/pull/188
 3. `wrangler dev`
 4. Open `http://127.0.0.1:8787/`
 5. Observe `__STATIC_CONTENT is not defined` error
+6. Observe that `env` contains an empty `__STATIC_CONTENT` object
+
+``` json
+{
+  "__STATIC_CONTENT": {}
+}
+```
--- workers-site/index.mjs
+++ workers-site/index.mjs
@@ -1,10 +1,11 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     const event = {
       request
     };
+    console.log(JSON.stringify(env))
     try {
       return await getAssetFromKV(event);
     } catch (e) {
